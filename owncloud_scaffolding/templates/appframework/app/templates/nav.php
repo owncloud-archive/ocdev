671abdb341dcb072a76e04862a7997537020bc14
Bernhard Posselt <nukeawhale@gmail.com>
2013-05-29 22:15:43 +0200

    fix navigation

--- owncloud_scaffolding/templates/appframework/app/templates/nav.php
+++ owncloud_scaffolding/templates/appframework/app/templates/nav.php
@@ -1,10 +1,8 @@
-<ul class="with-icon">
-	<li>
-		<a href="#" class="title">Level 1</a>
-		<ul>
-			<li>
-				<a href="#" class="title">Level 2</a>
-			</li>
-		</ul>
-	</li>
-</ul>
+<li class="open">
+	<a href="#" class="title">Level 1</a>
+	<ul>
+		<li>
+			<a href="#" class="title">Level 2</a>
+		</li>
+	</ul>
+</li>
