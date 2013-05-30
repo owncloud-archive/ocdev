68e7b4b002423f646a1de850f961a0f0f5b385d4
Bernhard Posselt <nukeawhale@gmail.com>
2013-05-30 16:00:59 +0200

    version bump

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ for dirpath, dirnames, filenames in os.walk(owncloud_dir):
 
 setup (
     name='owncloud_scaffolding',
-    version='0.1.3',
+    version='0.1.4',
     description='owncloud development scaffolding tool',
     author='Bernhard Posselt',
     author_email='nukewhale@gmail.com',
