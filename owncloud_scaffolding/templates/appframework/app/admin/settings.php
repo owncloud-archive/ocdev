f90956e88d79cfc9b2774b09ca59569b2c5ee451
Bernhard Posselt <nukeawhale@gmail.com>
2013-06-10 23:11:17 +0200

    use part method for returning the built output instead of printing it

--- owncloud_scaffolding/templates/appframework/app/admin/settings.php
+++ owncloud_scaffolding/templates/appframework/app/admin/settings.php
@@ -8,12 +8,5 @@ use OCA\AppFramework\App;
 use OCA\{{ app.namespace }}\DependencyInjection\DIContainer;
 
 
-// we need to fetch the output and return it for the admin page. Dont ask why
-ob_start();
+return App::part('SettingsController', 'index', array(), new DIContainer());
 
-App::main('SettingsController', 'index', array(), new DIContainer());
-
-$content = ob_get_contents();
-ob_clean();
-
-return $content;
