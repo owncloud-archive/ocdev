f295615ddf92032dd560cbfe4edc5a162f293916
Bernhard Posselt <dev@bernhard-posselt.com>
2014-05-06 01:15:42 +0200

    reuse requirements.txt file

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt', 'r') as infile:
+    install_requires = infile.read().split('\n')
+
+with open('README.rst', 'r') as infile:
+    long_description = infile.read()
+
 setup (
     name = 'ocdev',
     version = '0.0.4',
     description = 'ownCloud development tool',
-    long_description = open('README.rst').read(),
+    long_description = long_description,
     author = 'Bernhard Posselt',
     author_email = 'dev@bernhard-posselt.com',
     url = 'https://github.com/Raydiation/ocdev',
     packages = find_packages(),
     include_package_data = True,
     license = 'GPL',
-    install_requires = ['jinja2'],
+    install_requires = install_requires,
     keywords = ['owncloud', 'app', 'scaffolding', 'setup', 'development'],
     entry_points = {
         'console_scripts': [
