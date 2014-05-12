c7395b75380629befc05a38bc60cd6bc6ebda78a
Bernhard Posselt <dev@bernhard-posselt.com>
2014-05-12 02:15:28 +0200

    version bump

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,7 @@
+ocdev (0.0.5)
+
+* Add templates for ownCloud 7
+
 ocdev (0.0.4)
 
 * Move to setuptools for distributing
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'r') as infile:
 
 setup (
     name = 'ocdev',
-    version = '0.0.4',
+    version = '0.0.5',
     description = 'ownCloud development tool',
     long_description = long_description,
     author = 'Bernhard Posselt',
