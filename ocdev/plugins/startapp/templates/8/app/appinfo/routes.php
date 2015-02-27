3c3f6516cbb3a49efa2e92e8e2b484a043002b44
Bernhard Posselt <Raydiation@users.noreply.github.com>
2015-02-27 13:16:18 +0100

    Update routes.php

--- ocdev/plugins/startapp/templates/8/app/appinfo/routes.php
+++ ocdev/plugins/startapp/templates/8/app/appinfo/routes.php
@@ -4,7 +4,7 @@
 /**
  * Create your routes in here. The name is the lowercase name of the controller
  * without the controller part, the stuff after the hash is the method.
- * e.g. page#index -> OCA\{{ app.namespace }}\\Controller\PageController->index()
+ * e.g. page#index -> OCA\{{ app.namespace }}\Controller\PageController->index()
  *
  * The controller class has to be registered in the application.php file since
  * it's instantiated in there
