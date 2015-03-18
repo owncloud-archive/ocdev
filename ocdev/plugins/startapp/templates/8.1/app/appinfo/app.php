a39c959cb577d1d33655400ec03a498397ba01ee
Bernhard Posselt <dev@bernhard-posselt.com>
2015-03-18 10:50:13 +0100

    set 8.1 as default

--- ocdev/plugins/startapp/templates/8.1/app/appinfo/app.php
+++ ocdev/plugins/startapp/templates/8.1/app/appinfo/app.php
@@ -3,8 +3,14 @@
 
 namespace OCA\{{ app.namespace }}\AppInfo;
 
-\OC::$server->getNavigationManager()->add(function () {
-	$urlGenerator = \OC::$server->getURLGenerator();
+use OCP\AppFramework\App;
+
+$app = new App('{{ app.id }}');
+$container = $app->getContainer();
+
+$container->query('OCP\INavigationManager')->add(function () use ($container) {
+	$urlGenerator = $container->query('OCP\IURLGenerator');
+	$l10n = $container->query('OCP\IL10N');
 	return [
 		// the string under which your app will be referenced in owncloud
 		'id' => '{{ app.id }}',
@@ -22,7 +28,7 @@ namespace OCA\{{ app.namespace }}\AppInfo;
 
 		// the title of your application. This will be used in the
 		// navigation or on the settings page of your app
-		'name' => \OC::$server->getL10N('{{ app.id }}')->t('{{ app.name }}'),
+		'name' => $l10n->t('{{ app.name }}'),
 	];
 });
 
